408d1dc7a1f1dab4d53e8702b6b9e2b8f151fd2c
Caleb James Smith <colorado.caleb.smith@gmail.com>
2021-07-16 08:54:16 -0500

    update values for town house values

--- cost.py
+++ cost.py
@@ -147,14 +147,14 @@ def main():
     r = Rent(monthly_rent=1000.00)
     m = Mortgage(
                     num_years=nYears,
-                    purchase_price=150000.00,
-                    down_payment=30000.00,
+                    purchase_price=180000.00,
+                    down_payment=20000.00,
                     closing_costs=5000.0,
-                    annual_interest_rate=0.02440,
+                    annual_interest_rate=0.03000,
                     annual_prop_tax_rate=0.0133,
                     annual_insurance_rate=0.0042,
-                    monthly_hoa=0.00,
-                    selling_price=160000.00,
+                    monthly_hoa=133.33,
+                    selling_price=200000.00,
                     selling_realtor_fee_rate=0.06
                 )
     m.printInfo()
